516fd651c0d3e64ef99df14a9945c02058ebba1b
Kendra Chateau <Kendra@Kendras-Air.hitronhub.home>
2018-02-26 21:43:01 -0800

    math.abs to abs

--- app/main.py
+++ app/main.py
@@ -89,8 +89,8 @@ def findclosestfood(me,food):
 
 
 def findpointdistance(a,b):
-    xdiff = math.abs(a['x'] - b['x'])
-    ydiff = math.abs(a['y'] - b['y'])
+    xdiff = abs(a['x'] - b['x'])
+    ydiff = abs(a['y'] - b['y'])
 
     distance = math.sqrt(xdiff**2 + ydiff**2)
 
